c600ae876045bd1d62a5f378b1203ab6c8e79cea
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-11 19:15:23 +0000

    b

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
 """
-Python script
+This module contains a function that queries the Reddit API and
+ returns the number of subscribers for a given subreddit.
 """
-
-
 import requests
-import sys
 
 
-def subscribers_num(subreddit):
-    headers = {'User-Agent': 'Python:SubredditSubscriberCounter:v1.0 (by /u/1772hojaz)'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+def subscribers_(subreddit):
+    """
+    Queries the Reddit API for the number of subscribers for a given subreddit.
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception as e:
+    Args:
+        subreddit (str): The subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit,
+    or 0 if the subreddit is invalid.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'python3:0-subs:v1.0 (by /u/yourusername)'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get('data', {}).get('subscribers', 0)
+    else:
         return 0
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit_name = sys.argv[1]
-        subscribers = subscribers_num(subreddit_name)
-        print(f"{subscribers}")
 
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("pass an argument for the search.")
+    else:
+        print("{:d}".format(subscribers_num(sys.argv[1])))
