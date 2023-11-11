336ea35ef114c5d33a82293a40d5e00b2711c536
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2023-11-11 16:14:01 +0200

    correction of file 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 
+"""
+This module provides a function `subscribers_num` that queries the Reddit API to determine the number
+of subscribers for a specified subreddit. If the subreddit is valid, it returns the total number of subscribers.
+In case of an invalid subreddit or an error in the request, it returns 0.
 
+The module is designed to be run as a script, taking the subreddit name as a command-line argument.
 """
-A function that queries the Reddit API and returns the number of subscribers
-(total subsribers
-"""
+
 import requests
 import sys
 
 
 def subscribers_num(subreddit):
-    headers = {'User-Agent': 'Python:SubredditSubscriberCounter:v1.0(by /1772hojaz)'}
+    """
+    Query the Reddit API to get the number of subscribers for a given subreddit.
+
+    Args:
+    subreddit (str): The name of the subreddit.
+
+    Returns:
+    int: The number of subscribers if the subreddit is valid, otherwise 0.
+    """
+    headers = {'User-Agent': 'Python:SubredditSubscriberCounter:v1.0 (by /u/1772hojaz)'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     try:
-        # Making a GET request to the Reddit API
         response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # If the response status code is 200 (OK), extract the number of subscribers
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
@@ -33,3 +42,4 @@ if __name__ == '__main__':
         subreddit_name = sys.argv[1]
         subscribers = subscribers_num(subreddit_name)
         print(f"{subscribers}")
+
