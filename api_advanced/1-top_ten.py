4136f84c096cd21e64f7317e3ba00837a3815d05
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-11 22:16:12 +0000

    added file 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""
-Python script that returns the titles of the first 10 hot posts
-"""
+"""this module returns the titles of the top 10 hot posts"""
 import requests
 
 
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'python:top_ten:v1.0 (by /u/yourusername)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """this function returns the titles of the top 10 hot posts"""
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code != 200:
-        print(None)
-        return
-
-    try:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts[:10]:
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
             print(post['data']['title'])
-    except Exception as e:
-        print(None)
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
     else:
-        top_ten(sys.argv[1])
+        print(None)
