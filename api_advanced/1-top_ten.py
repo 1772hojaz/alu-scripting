0f5b4ac484912808d2b88cfb611e3316e3ba19e6
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-11 22:32:44 +0000

    eish

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,25 +4,16 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
-    subreddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-agent': 'Mozilla/5.0'}
+    """this function returns the titles of the top 10 hot posts"""
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(subreddit_url, headers=headers)
 
-    try:
-        response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            for post in data['data']['children']:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.RequestException:
-        print(None)
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
     else:
-        top_ten(sys.argv[1])
+        print(None)
+        
