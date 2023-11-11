0f0c1169a82793eeca7abf6d8d1d98fac9176c7f
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-11 21:54:28 +0000

    corrected the PEP8

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,7 +9,8 @@ def number_of_subscribers(subreddit):
         .format(subreddit)
 
     header = {
-        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+        '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(reddit_url,
                             headers=header
                             )
