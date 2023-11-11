7ef136461a5073e53da4030c894cd494b489608a
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-11 23:25:33 +0000

    yho

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this module returns the titles of the top 10 hot posts"""
 import requests
+import sys
 
 
 def top_ten(subreddit):
@@ -10,11 +11,18 @@ def top_ten(subreddit):
     headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
-            return  "OK"
+    try:
+        if response.status_code == 200:
+            data = response.json()['data']
+            for post in data['children'][:10]:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except request.RequestException as e:
+        print(None)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        top_ten(sys.argv[1])
     else:
         print(None)
-        return "None"
