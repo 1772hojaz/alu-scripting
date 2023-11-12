3824953e4d47f34a7272f14b029c5de2b9fcdd2e
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-12 07:12:52 +0000

    mop

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,27 +5,16 @@ import sys
 
 
 def top_ten(subreddit):
-    """this function returns the titles of the top 10 hot posts"""
-    subreddit_url = "https://www.reddit.com/r/{}/hot.json" \
+    """Function that queries the Reddit API."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
         .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(subreddit_url, headers=headers)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
 
-    try:
-        if response.status_code == 200:
-            data = response.json().get('data', {}).get('children')
-            for post in data:
-                print(post.get('data').get('title',))
-        else:
-            print(None)
-    except request.RequestException as e:
-        print(None)
-
-if __name__ == "__main__":
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-        sys.exit(1)
+    if response.status_code == 200:
+        data = response.json().get('data').get('children')
+        for post in data:
+            print(post.get('data').get('title'))
     else:
-        top_ten(sys.argv[1])
-    
+        print(None)
+        
