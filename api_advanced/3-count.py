13c04f860a75f25e37ed1537b9fef9391eb87825
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-12 16:30:04 +0000

    final 112

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" 3-count.py """
+"""the function that queries the Reddit API"""
 import json
 import requests
 
 
 def count_words(subreddit, word_list, after="", count=[]):
-    """ prints a sorted count of given keywords """
+    """ the actual function that counts the occurrences of words """
 
     if after == "":
         count = [0] * len(word_list)
