49868031773bbc86e6001d3cc250cf7195b2e1da
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-12 00:28:28 +0000

    po5r

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,18 +13,19 @@ def top_ten(subreddit):
 
     try:
         if response.status_code == 200:
-            data = response.json()['data']
-            for post in data['children'][:10]:
-                print(post['data']['title'])
+            data = response.json().get('data', {}).get('children')
+            for post in data:
+                print(post.get('data').get('title',))
         else:
             print(None)
     except request.RequestException as e:
         print(None)
-        print(str(e))
 
 if __name__ == "__main__":
     top_ten = __import__('1-top_ten').top_ten
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
+        sys.exit(1)
     else:
         top_ten(sys.argv[1])
+    
