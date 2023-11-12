cfeb43f6abaa3e9f7bf1dd8514ad9a095c95e475
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2023-11-12 07:25:13 +0000

    added

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,7 +5,9 @@ import sys
 
 
 def top_ten(subreddit):
-    """Function that queries the Reddit API."""
+    """Function that queries the Reddit API
+    and and prints the titles of the first 10
+    hot posts listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
         .format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
@@ -17,4 +19,3 @@ def top_ten(subreddit):
             print(post.get('data').get('title'))
     else:
         print(None)
-        
